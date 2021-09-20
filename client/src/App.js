2b36909b61cbc093aac295ab059c22bf266cc19c
GriffinDZ <45401628+GriffinDZ@users.noreply.github.com>
2021-09-19 21:00:11 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import GeoMap from "./components/geomap.js";
+import data from "./geography_data/us.geo.json";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  // Uses the year prior to the current one because the map uses data which is compiled at the end of each year
+  var currYear = new Date();
+  currYear.setFullYear(currYear.getFullYear() - 1);
+  const finalYearData = currYear.getFullYear();
+  const [year, setYear] = useState(finalYearData);
+
+  return (
+    <React.Fragment>
+      <div id="app-container" className="container-fluid d-flex flex-column justify-content-end">
+        <div className="pt-2 mb-2 row">
+          <div className="col justify-content-center text-center">
+            <h1>Honey Bee Colonies in the United States</h1>
+          </div>
+        </div>
+        <GeoMap className="mb-auto" data={data} year={year} />
+        <div className="row mt-2 justify-content-center">
+          <div className="col text-center">
+            <h3>Select a year to highlight. Current year: <u>{year}</u></h3>
+          </div>
+        </div>
+        <div className="row mt-2 justify-content-center mb-2">
+          <div className="col-sm-8 col-auto-12 justify-content-center d-flex flex-row">
+            <span><i>1987</i></span>
+            <input
+              type="range"
+              className="form-range mx-3"
+              min="1987"
+              max={finalYearData}
+              step="1"
+              value={year}
+              onChange={event => setYear(event.target.value)}>
+            </input>
+            <span><i>{finalYearData}</i></span>
+          </div>
+        </div>
+        <div id="footer" className="row bg-dark justify-content-end py-2">
+          <div id="footer-col-1" className="col-sm-6">
+            <small id="credits">Developed by <a id="personalLink"
+              href="https://griffinzody.com/" rel="noopener noreferrer" target="_blank">Griffin Zody</a>. Github <a id="personalLink"
+                href="https://github.com/GriffinDZ/bee-website" rel="noopener noreferrer" target="_blank">here</a>.</small>
+          </div>
+          <div id="footer-col-2" className="col-sm-6">
+            <small id="NASS-Disclaimer"><i>This product uses the <a rel="noopener noreferrer" target="_blank" id="NASS-Link" href="https://quickstats.nass.usda.gov/api">
+              NASS API</a> but is not endorsed or certified by NASS.</i></small>
+          </div>
+        </div>
+      </div>
+    </React.Fragment >
+  );
+}
+
+export default App;
---
+++ client/src/components/geomap.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { select, geoPath, geoAlbersUsa, scaleSequential, interpolateRgb } from 'd3';
+import axios from 'axios';
+import useResizeObserver from "../utils/useResizeObserver.js";
+import { throttle } from 'lodash';
+
+function GeoMap({ data, year }) {
+    const svgRef = useRef();
+    const wrapperRef = useRef();
+    const labelRef = useRef();
+    const isYearDataLoaded = useRef(false);
+    const [yearData, setYearData] = useState([]);
+    const [selectedState, setSelectedState] = useState(null);
+    const dimensions = useResizeObserver(wrapperRef);
+
+    useEffect(function getYearDataFromAPI() {
+        async function fetchYearData() {
+            isYearDataLoaded.current = false;
+            await axios.get(`http://localhost:5000/${year}`)
+                .then(response => {
+                    setYearData(response.data[0].state_values);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            isYearDataLoaded.current = true;
+        };
+        fetchYearData();
+    }, [year]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(throttle(function DrawMap() {
+        const svg = select(svgRef.current);
+
+        const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect();
+
+        const projection = geoAlbersUsa().fitSize([width, height * 0.98], selectedState || data).precision(25);
+        const pathGenerator = geoPath().projection(projection);
+
+        svg
+            .selectAll(".state")
+            .data(data.features)
+            .join("path")
+            .on("click", function (e, feature) {
+                setSelectedState(selectedState === feature ? null : feature);
+            })
+            .attr("class", "state")
+            .transition().duration(1000)
+            .attr("d", feature => pathGenerator(feature));
+
+    }, 1000), [data, selectedState, dimensions]);
+
+    useEffect(function colorStates() {
+        if (isYearDataLoaded.current) {
+            const svg = select(svgRef.current);
+
+            const colorScale = scaleSequential(interpolateRgb("#fff5cc", "#E3AE00")).domain([0, 5]);
+
+            svg
+                .selectAll(".state")
+                .transition().duration(150)
+                .attr("fill", feature => {
+                    const currYear = yearData.find(el => el.state === feature.properties["NAME"].toUpperCase());
+                    if (currYear !== undefined) {
+                        return colorScale(currYear.total_bee_colonies / feature.properties["CENSUSAREA"]);
+                    }
+                    else {
+                        return "#e6e6e6";
+                    }
+                });
+        }
+    }, [data, yearData]);
+
+    useEffect(function setLabel() {
+        const label = select(labelRef.current);
+
+        label
+            .selectAll(".label")
+            .data([selectedState])
+            .join("text")
+            .attr("class", "label")
+            .text(
+                feature => {
+                    if (feature && isYearDataLoaded) {
+                        const currYear = yearData.find(el => el.state === feature.properties["NAME"].toUpperCase());
+                        if (currYear !== undefined) {
+                            return feature.properties["NAME"] + ": " + (currYear.total_bee_colonies
+                                / feature.properties["CENSUSAREA"]).toFixed(2) + " colonies per square mile.";
+                        }
+                        else {
+                            return feature.properties["NAME"] + ": Insignificant data this year.";
+                        }
+                    }
+                    else {
+                        return "Click a state to see its data.";
+                    }
+                }
+            )
+    }, [yearData, selectedState]);
+
+    return (
+        <React.Fragment>
+            <div id="map-wrapper" ref={wrapperRef}>
+                <svg ref={svgRef}></svg>
+            </div>
+            <div className="text-center">
+                <p id="map-label-1" className="m-1" ref={labelRef}></p>
+                <p id="map-label-2" className="m-1"><i>(Click the selected state again to zoom out.)</i></p>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default GeoMap;
